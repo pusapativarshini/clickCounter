a52a7193009ac12f6d527d961b730172b0fb681c
goranti-maneesh <gorantimaneeshreddy@gmail.com>
2022-11-30 16:34:25 +0000

    issue in click counter

--- src/components/ClickCounter/index.js
+++ src/components/ClickCounter/index.js
@@ -1,10 +1,11 @@
-// Write your code here
+/* eslint-disable react/no-unknown-property */
 import './index.css'
 import {Component} from 'react'
 
 class ClickCounter extends Component {
   state = {count: 0}
-  onIncrement = () => {
+
+  onIncrment = () => {
     this.setState(prevstate => ({count: prevstate.count + 1}))
   }
 
@@ -13,11 +14,11 @@ class ClickCounter extends Component {
     return (
       <div className="container">
         <h1 className="heading">
-          This Button has been clicked <span className="span1">{count}</span>
+          This Button has been clicked <span className="span1">{count} </span>
           times
         </h1>
         <p>Click the button to increase the count </p>
-        <button className="btn" onClick={this.onIncrement}>
+        <button className="btn" type="button" onclick={this.onIncrement}>
           Click Me
         </button>
       </div>
